b2e31a01832f352b7f414f3e6c6f8f53f0f93366
Preeti yadav <preetiyadav30430@gmail.com>
2024-06-05 19:29:58 +0530

    added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import video1 from "../assets/video1.mp4";
-import video2 from "../assets/video2.mp4";
+import video1 from "../assets/video/video1.mp4";
+import video2 from "../assets/video/video2.mp4";
 
 function HeroSection() {
   return (
